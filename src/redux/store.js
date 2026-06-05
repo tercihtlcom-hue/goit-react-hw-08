e5ebc02f20bcff2564ae255f0ce9b0e7a4929df2
Arif Yazici <tercihtl.com@gmail.com>
2026-06-05 16:31:54 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from './auth/slice';
-import contactsReducer from './contacts/slice';   // ✅ DÜZELTİLDİ (auth yerine contacts)
+import contactsReducer from './contacts/slice';
 import filtersReducer from './filters/slice';
 
 const authPersistConfig = {
